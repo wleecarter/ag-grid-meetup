5e89a892bdd50773f18e23437ad97f3f7789f8d9
Lee Carter <wleecarter@gmail.com>
2020-11-11 11:30:27 -0500

    change column order, format peak chart prop

--- src/app/column-defs/06-column-filters.ts
+++ src/app/column-defs/06-column-filters.ts
@@ -43,11 +43,11 @@ export const gridOptions = {
       field: 'certification',
     },
     {
-      field: 'peakChartPositionUS',
-      filter: 'agNumberColumnFilter',
+      field: 'albumType',
     },
     {
-      field: 'albumType',
+      field: 'peakChartPositionUS',
+      filter: 'agNumberColumnFilter',
     },
     {
       field: 'unitsSold',
