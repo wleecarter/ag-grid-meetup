82052c748f733c6c913b290828514bf89d4ecb23
Lee Carter <wleecarter@gmail.com>
2020-11-10 21:15:21 -0500

    angular material setup

---
+++ src/app/material.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatPaginatorModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatListModule,
+  MatTableModule,
+  MatToolbarModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [modules],
+  exports: [modules],
+})
+export class MaterialModule {}
